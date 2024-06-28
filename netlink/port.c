014974a2c8138790b478756f28260eb067bf2065
Maxime Chevallier <maxime.chevallier@bootlin.com>
2024-06-28 16:27:07 +0200

    netlink: port: Fix reporting for link state and speed
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>

--- netlink/port.c
+++ netlink/port.c
@@ -75,11 +75,11 @@ int port_reply_cb(const struct nlmsghdr *nlhdr, void *data)
 
 	if (tb[ETHTOOL_A_PORT_LINK])
 		print_string(PRINT_ANY, "link", "link : %s\n", port_link_to_str(
-							mnl_attr_get_u32(
-								tb[ETHTOOL_A_PORT_LINK])));
+							mnl_attr_get_u8(tb[ETHTOOL_A_PORT_LINK])));
 
+	/* TODO: Report by converting the  */
 	if (tb[ETHTOOL_A_PORT_SPEED])
-		show_u32("speed", "speed\n", tb[ETHTOOL_A_PORT_SPEED]);
+		show_u32("speed", "speed : ", tb[ETHTOOL_A_PORT_SPEED]);
 
 	if (!silent)
 		print_nl();
